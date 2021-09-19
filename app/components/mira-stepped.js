9cf4ebdd29a72b1d13e1f5a5fa2b6ef9c529d05e
nickschot <nickschot@gmail.com>
2021-09-19 19:42:55 +0200

    work on presentation

--- app/components/mira-stepped.js
+++ app/components/mira-stepped.js
@@ -1,6 +1,18 @@
 import Component from '@glimmer/component';
+import { task, timeout } from 'ember-concurrency';
+import { tracked } from '@glimmer/tracking';
 
 export default class MiraSteppedComponent extends Component {
+  @tracked eyesClosed = false;
+  @tracked _look;
+  @tracked _jump = false;
+
+  constructor() {
+    super(...arguments);
+
+    this.blinkTask.perform();
+  }
+
   get classNames() {
     let classes = [];
 
@@ -9,5 +21,17 @@ export default class MiraSteppedComponent extends Component {
     }
 
     return classes.join(' ');
+  }
+
+  @task
+  *blinkTask() {
+    while (true) {
+      yield timeout(2500 + Math.random() * 2500);
+      if (this.args.step > 30) {
+        this.eyesClosed = true;
+        yield timeout(250);
+        this.eyesClosed = false;
+      }
+    }
   }
 }
--- app/controllers/creation.js
+++ app/controllers/creation.js
@@ -7,7 +7,9 @@ import { wait } from 'ember-animated';
 import { assert } from '@ember/debug';
 
 const CODE_SNIPPETS = [
+  // css
   {
+    title: 'Basic shape',
     snippet: `.mira {
     border-radius: 50% 50% 50% 50% / 60% 60% 40% 40%;
   }`,
@@ -15,6 +17,7 @@ const CODE_SNIPPETS = [
     steps: [2, 3],
   },
   {
+    title: 'Body',
     snippet: `.body {
     width: 100%;
     height: 100%;
@@ -24,6 +27,7 @@ const CODE_SNIPPETS = [
     steps: [5, 6],
   },
   {
+    title: 'Body',
     snippet: `.body {
     border-radius: 50% 50% 50% 50% / 60% 60% 40% 40%;
     box-shadow: inset 0 0 1px 0 rgba(255, 255, 255, 0.25),
@@ -33,6 +37,7 @@ const CODE_SNIPPETS = [
     steps: [7, 8],
   },
   {
+    title: 'Head',
     snippet: `.head {
     position: absolute;
     background: #FFF;
@@ -45,6 +50,7 @@ const CODE_SNIPPETS = [
     steps: [9, 10],
   },
   {
+    title: 'Head',
     snippet: `.head {
     ...
 
@@ -57,6 +63,7 @@ const CODE_SNIPPETS = [
     steps: [11, 12],
   },
   {
+    title: 'Head',
     snippet: `.head {
     border-radius: 0 0 50% 50% / 0 0 20% 20%;
   }`,
@@ -64,6 +71,7 @@ const CODE_SNIPPETS = [
     steps: [13, 14],
   },
   {
+    title: 'Head',
     snippet: `.mira {
     overflow: hidden;
   }`,
@@ -71,6 +79,7 @@ const CODE_SNIPPETS = [
     steps: [15, 16],
   },
   {
+    title: 'Finishing touches',
     snippet: `.head {
     box-shadow: 0 0 2px 0 rgba(0,0,0,0.5),
                 inset 0 0 5px 0 rgba(0,0,0,0.5);
@@ -79,6 +88,7 @@ const CODE_SNIPPETS = [
     steps: [17, 18],
   },
   {
+    title: 'Finishing touches',
     snippet: `.body {
     transform: scale(0.975);
   }`,
@@ -86,6 +96,7 @@ const CODE_SNIPPETS = [
     steps: [19, 20],
   },
   {
+    title: 'Eyes',
     snippet: `.eye {
     --eye-size: calc(var(--mira-size) * 0.08);
 
@@ -108,23 +119,175 @@ const CODE_SNIPPETS = [
     steps: [21, 22],
   },
   {
+    title: 'Eyes',
     snippet: `.eye {
     border-radius: 100%;
   }`,
     language: 'css',
     steps: [23, 24],
   },
+  // animations
+  {
+    title: 'Animating Mira',
+    steps: [25],
+  },
+  {
+    title: 'Blinking - CSS Transitions',
+    steps: [26],
+    list: [
+      'Transition between states',
+      'Runs with state changes of CSS selector',
+      'Adding/removing a class',
+      'Pseudo-selectors like :hover',
+    ],
+  },
+  {
+    title: 'Blinking - CSS Transitions',
+    steps: [27],
+    language: 'css',
+    snippet: `.eye {
+    box-shadow: inset 0 0 0 rgba(255,255,255,0.85);
+  }
+
+  .eye.closed {
+    box-shadow:
+     inset 0 var(--eye-size) var(--eye-size) var(--eye-size) #fff;
+  }
+  `,
+  },
+  {
+    title: 'Blinking - CSS Transitions',
+    steps: [28, 29],
+    language: 'css',
+    snippet: `.eye {
+    box-shadow: inset 0 0 0 rgba(255,255,255,0.85);
+    transition: box-shadow 150ms ease;
+  }
+
+  .eye.closed {
+    box-shadow:
+     inset 0 var(--eye-size) var(--eye-size) var(--eye-size) #fff;
+  }
+  `,
+  },
+  {
+    title: 'Blinking - ember-concurrency',
+    steps: [30],
+    language: 'javascript',
+    snippet: `export default class Mira extends Component {
+    @tracked eyesClosed = false;
+
+    constructor() {
+      super(...arguments);
+
+      this.blinkTask.perform();
+    }
+
+    @task
+    *blinkTask() {
+      while (true) {
+        yield timeout(2500 + Math.random() * 2500);
+        this.eyesClosed = true;
+        yield timeout(250);
+        this.eyesClosed = false;
+      }
+    }
+  }`,
+  },
+  {
+    title: 'Blinking - ember-concurrency',
+    steps: [31, 32],
+    language: 'handlebars',
+    snippet: `<div class="head">
+    <div class="eye left {{if this.eyesClosed "closed"}}"></div>
+    <div class="eye right {{if this.eyesClosed "closed"}}"></div>
+  </div>`,
+  },
+  {
+    title: 'CSS Transitions',
+    steps: [33],
+    list: ['Simple', 'Performant', 'Limited control'],
+  },
+  {
+    title: 'Body - CSS Animations',
+    steps: [34],
+    list: [
+      'More control over timing and easing',
+      'Allows more complicated effects',
+      'Specification through keyframes',
+    ],
+  },
+  {
+    title: 'Body - CSS Animations',
+    steps: [35, 36],
+    language: 'css',
+    snippet: `@keyframes pulse-background {
+    from {
+      background: red;
+    }
+
+    to {
+      background: blue;
+    }
+  }
+
+  .body {
+    animation: background-pulse infinite 4s
+  }
+  `,
+  },
+  {
+    title: 'Body - CSS Animations',
+    steps: [37, 38],
+    language: 'css',
+    snippet: `@keyframes background-pulse {
+    0% {
+      background: red;
+    }
+
+    50% {
+      background: blue;
+    }
+
+    100% {
+      background: red;
+    }
+  }
+
+  .body {
+    animation: background-pulse infinite 4s
+  }
+  `,
+  },
+  {
+    title: 'Body - CSS Animations',
+    steps: [39, 40],
+    language: 'css',
+    snippet: `/* animate amount of blur on the white box-shadow */
+  @keyframes pulse {
+    0% {
+      box-shadow: inset 0 0 1px 0 rgba(255,255,255,0.25),
+                  inset 0 0 calc(var(--mira-size) * 0.65) 0 #fff;
+    }
+    50% {
+      box-shadow: inset 0 0 1px 0 rgba(255,255,255,0.25),
+                  inset 0 0 calc(var(--mira-size) * 0.25) 0 #fff;
+    }
+    100% {
+      box-shadow: inset 0 0 1px 0 rgba(255,255,255,0.25),
+                  inset 0 0 calc(var(--mira-size) * 0.65) 0 #fff;
+    }
+  }`,
+  },
 ];
 
 const CODE_SNIPPETS_PER_STEP = CODE_SNIPPETS.reduce(
-  (result, { snippet, language, steps }) => {
+  (result, { steps, ...rest }) => {
     for (let step of steps) {
       assert(`Step ${step} was already defined!`, !result.has(step));
 
-      result.set(step, { snippet, language });
+      result.set(step, { ...rest });
     }
-
-    console.log(result);
     return result;
   },
   new Map()
@@ -134,15 +297,15 @@ export default class CreationController extends Controller {
   queryParams = ['step'];
   @tracked step = 0;
 
-  *transition({ insertedSprites, removedSprites, keptSprites }) {
+  fade = fade;
+
+  *transition({ insertedSprites, removedSprites }) {
     if (insertedSprites.length) {
-      //yield wait(500);
+      yield wait(500);
       insertedSprites.forEach(fadeIn);
     } else {
       removedSprites.forEach(fadeOut);
     }
-
-    keptSprites.forEach(fade);
   }
 
   get currentCodeSnippet() {
